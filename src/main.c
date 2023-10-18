44cb22097259980daf30931ef40f00b4808119ab
A-Mahla <>
2023-10-18 02:34:10 +0200

    malloc implementation with structure

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amahla <ammah.connect@outlook.fr>       +#+  +:+    +#+     +#+      */
 /*                                             +#+    +#+   +#+     +#+       */
 /*   Created: 2023/10/17 13:35:00 by amahla  #+#      #+#  #+#     #+#        */
-/*   Updated: 2023/10/18 02:25:24 by amahla ###       ########     ########   */
+/*   Updated: 2023/10/18 02:26:00 by amahla ###       ########     ########   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,6 @@
 
 int	main(void)
 {
-	for (int i = TINY; i <= LARGE; i++) {
-		switch (i) {
-		case TINY:
-			printf("type\t: TINY\n");
-			break;
-		case SMALL:
-			printf("type\t: SMALL\n");
-			break;
-		case LARGE:
-			printf("type\t: LARGE\n");
-			break;
-		}
-		msegment[i].next = malloc(sizeof(malloc_segment_t *));
-		printf("size\t:%zu\nnext\t:%zu\nstart\t:%zu\n", sizeof(msegment[i].size), sizeof(msegment[i].start), sizeof(msegment[i].next));
-		printf("%zu", HEADER_SIZE);
-	}
 	ft_malloc(16);
 	return 0;
 }
