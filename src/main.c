c86616c393f31a6ba636f1c75f82bf832aa7d95c
Amir MAHLA <amahla@made-f0Br1s3.clusters.42paris.fr>
2023-10-19 15:48:16 +0200

    Malloc : handle fragmentation => free_memory() / free() in progress

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amahla <ammah.connect@outlook.fr>       +#+  +:+    +#+     +#+      */
 /*                                             +#+    +#+   +#+     +#+       */
 /*   Created: 2023/10/17 13:35:00 by amahla  #+#      #+#  #+#     #+#        */
-/*   Updated: 2023/10/19 15:43:16 by amahla           ###   ########.fr       */
+/*   Updated: 2023/10/19 15:48:13 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,17 @@ int	main(void)
 	char *test1 = ft_malloc(16);
 	if (!test1)
 		printf("test1 : WHAT !!!!");
+	for (int i = 0; i < 15; i++)
+		test1[i] = 'A';
+	test1[15] = '\0';
+	printf("%s", test1);
 	char *test2 = ft_malloc(16);
 	if (!test2)
 		printf("test2 : WHAT !!!!");
+	for (int i = 0; i < 15; i++)
+		test2[i] = 'B';
+	test2[15] = '\0';
+	printf("%s", test2);
 	
 	ft_free(test1);
 	ft_free(test2);
